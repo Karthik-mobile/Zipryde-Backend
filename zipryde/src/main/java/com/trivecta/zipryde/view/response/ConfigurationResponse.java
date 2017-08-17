914fd29d7fc430bd6a9ad89f6aca29d9ccafcb7d
suganya-trivecta <suganya.pa@trivectadigital.com>
2017-08-17 11:04:23 +0530

    Added Configuration Request and Response

---
+++ zipryde/src/main/java/com/trivecta/zipryde/view/response/ConfigurationResponse.java
@@ -0,0 +1,48 @@
+package com.trivecta.zipryde.view.response;
+
+public class ConfigurationResponse {
+	
+	private Integer id;
+	
+	private String url;
+	
+	private String accessKey;
+		
+	private String type;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getAccessKey() {
+		return accessKey;
+	}
+
+	public void setAccessKey(String accessKey) {
+		this.accessKey = accessKey;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+
+
+}
